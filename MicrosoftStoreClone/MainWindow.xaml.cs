58b7b4f525bfb8026050ae397bdad631656d6c3c
Marcin Krupiński <markru14@st.amu.edu.pl>
2021-11-03 12:06:34 +0100

    Add MainPage + UserControls to shop

--- MicrosoftStoreClone/MainWindow.xaml.cs
+++ MicrosoftStoreClone/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MicrosoftStoreClone.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,16 @@ namespace MicrosoftStoreClone
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Main MainWindowContentPage;
         public MainWindow()
         {
             InitializeComponent();
+            MainWindowContentPage = new Main();
+        }
+
+        private void MainWindowFrame_Loaded(object sender, RoutedEventArgs e)
+        {
+            MainWindowFrame.Content = MainWindowContentPage;
         }
     }
 }
